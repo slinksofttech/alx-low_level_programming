188107bfde2e7ac87f9b88e75a9819d42602fdfa
Samson <slinksofttech@gmail.com>
2022-09-09 09:11:25 -0700

    C programming is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+{ #include <stdio.h>
 
-int main(void)
-{
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-	return (1)
-}
+
+
+	int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+
+
+		return (1);
+
+	} }

cf1fc4368e7843cdc1e41e9771172b1e4e1e3597
Samson <slinksofttech@gmail.com>
2022-09-08 19:09:33 -0700

    I am coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,14 @@
-#include <stdio.h>                                                                                                            
-                                                                                                                           
-/**                                                                                                                           
+#include <stdio.h>
 
-* main - print the string in the put function                                                                                 
-
-*                                                                                                                             
-
-* Descriptionm: using the main function                                                                                       
-
-* this program prints "Programming is like building a multilingual puzzle                                                     
-
-* Return: 0
-* **/                                                                                                                           
-
-int main(void)                                                                                                                
-
-{                                                                                                                             
-
-        puts("\"Programming is like building a multilingual puzzle");                                                         
-
-        return (0);                                                                                                        
-
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ * **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

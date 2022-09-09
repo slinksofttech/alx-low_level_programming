da302db08be9e50c56fea685269011e690984f50
Samson <slinksofttech@gmail.com>
2022-09-09 05:42:30 -0700

    I am coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,11 @@
  * **/
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
 
-	printf("Size of char: %d 1 byte(s)\n", sizeof(c));
-	printf("Size of int: %d 4 byte(s)\n", sizeof(i));
-	printf("Size of long int: %d 4 byte(s)\n", sizeof(li));
-	printf("Size of long long int: %d 8 byte(s)\n", sizeof(lli));
-	printf("Size of float: %d 4 byte(s)\n", sizeof(f));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

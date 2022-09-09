86564c3cd510437f15079a13ee86e75af2ac4c8b
Samson <slinksofttech@gmail.com>
2022-09-08 19:00:20 -0700

    I am coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,16 +10,15 @@
 
 * this program prints "Programming is like building a multilingual puzzle                                                     
 
-* Return: 0                                                                                                                   
-
-*/                                                                                                                            
+* Return: 0
+* **/                                                                                                                           
 
 int main(void)                                                                                                                
 
 {                                                                                                                             
 
-        puts("\"Programming is like building a multilingual puzzle"):                                                         
+        puts("\"Programming is like building a multilingual puzzle");                                                         
 
-        return (0):                                                                                                        
+        return (0);                                                                                                        
 
 }

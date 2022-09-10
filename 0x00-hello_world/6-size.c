3428cad8fe1bdda2e2d6ef4065eb592d460f7a07
Samson <slinksofttech@gmail.com>
2022-09-10 09:25:05 -0700

    Coding is so so cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,12 @@
  * and run on
  * Return: Always 0 (Success)
  * **/
+
 int main(void)
 {
-
-
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 	return (0);

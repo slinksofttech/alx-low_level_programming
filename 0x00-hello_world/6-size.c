16d9e47311c36978d5a532d6bd1f52569cf9bdaf
Samson <slinksofttech@gmail.com>
2022-09-10 06:15:50 -0700

    Coding is cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 
 
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a long long int: %ld byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", (int) sizeof(float));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 	return (0);
 }

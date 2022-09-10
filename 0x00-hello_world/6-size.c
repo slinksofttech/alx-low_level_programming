b40bfca2d96acf96840092bf4dbe2dbbe2914941
Samson <slinksofttech@gmail.com>
2022-09-10 06:34:00 -0700

    Coding is cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,13 @@
  * Description: prints the size of various types on the computer it is complied
  * and run on
  * Return: Always 0 (Success)
- **/
+ * **/
 int main(void)
 {
 
 
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));

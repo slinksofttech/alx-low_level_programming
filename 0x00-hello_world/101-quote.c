469a7d2f919728c570e0d7dd0a0a90fc88dae90d
Samson <slinksofttech@gmail.com>
2022-09-11 11:52:40 -0700

    Ending Hello world coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include <unistd.h>
 
 
@@ -12,7 +10,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 

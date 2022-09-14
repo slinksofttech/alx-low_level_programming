b13e477d25fc4a9534ff5892cb42a86438a1724e
Samson <slinksofttech@gmail.com>
2022-09-13 21:06:06 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char);
 void print_alphabet(void);
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,4 +15,6 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
+
+#endif
 

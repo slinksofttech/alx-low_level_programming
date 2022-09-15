725be82fe510fa0828a66a6c68f50317dd16a29b
Samson <slinksofttech@gmail.com>
2022-09-15 03:05:45 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Print 3-islower.c -function to print
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

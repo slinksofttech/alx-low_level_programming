591785596e04c37f72d3a18f9c3099581fe4ea66
Samson <slinksofttech@gmail.com>
2022-09-18 11:42:09 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	int n, sum = 0;
+
 	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3) == 0 || (n % 5) == 0)
+	if ((n % 3) == 0 || (n % 5) == 0)
 	sum += n;
 	}
 	printf("%d\n", sum);

35fd6e081dfc7136c014317f70346b54a4901c39
Samson <slinksofttech@gmail.com>
2022-09-18 10:11:25 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,6 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
@@ -23,7 +22,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + '0');
 			
-			_putchar((prod % 10) + '0');
+				_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}

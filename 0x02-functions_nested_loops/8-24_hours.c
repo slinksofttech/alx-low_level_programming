955358431f9ba2423a4cada44ea014734ee79246
Samson <slinksofttech@gmail.com>
2022-09-18 08:58:29 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,19 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_reminder;
-	int mins_remainder;
+	int mins_reminder;
 
 	while (hours <= 23)
 	{
 	while (minutes <= 59)
 	{
 av
-	hours_remainder = hours % 10;
+	hours_reminder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
+	_putchar(hours_reminder + '0');
 	_putchar(':');
 	_putchar(minutes / 10 +'0');
-	_putchar(mins_remainder + '0');
+	_putchar(mins_reminder + '0');
 	minutes++;
 	_putchar('\n');
 	}

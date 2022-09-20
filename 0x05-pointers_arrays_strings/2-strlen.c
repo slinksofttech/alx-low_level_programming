1d6f81382d564d36004a6f1bda8df2c78535fc46
Samson <slinksofttech@gmail.com>
2022-09-19 19:29:46 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 
 /**
 *_strlen - returns the length of a string
-* @s: string
+*@s: string
 *Return: return length as integers;
 */
 
 int _strlen(char *s)
 {
 	int len = 0;
+	
 	while (*(s + len) != '\n')
 		len++;
+	
 	return (len);
 }

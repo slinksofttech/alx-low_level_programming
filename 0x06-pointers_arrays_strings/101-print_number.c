bb7acdde27dbe7d22defe6e4e0b2a37413b68dfb
Samson <slinksofttech@gmail.com>
2022-09-23 18:29:36 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,6 +11,7 @@
 void print_number(int n)
 {
 	unsigned int m, d, count;
+	
 	if (n < 0)
 		{
 			_putchar(45);
@@ -29,6 +30,6 @@ void print_number(int n)
 		}
 		for (; count >= 1; count /= 10)
 		{
-													_putchar(((m / count) % 10) + 48);
-												}
+			_putchar(((m / count) % 10) + 48);
+		}
 }

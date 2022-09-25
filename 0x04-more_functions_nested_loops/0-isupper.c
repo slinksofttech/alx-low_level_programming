fbd20e5f7282d98445a8af2cc705a6c56fe1128f
Samson <slinksofttech@gmail.com>
2022-09-25 15:53:36 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,10 +8,12 @@
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	return (0);
 
-	return (c >= 65 && c <= 9
-			return (c >= 'A' && c <= 'Z');
+
+	else
+		
+		return (0);
 }
